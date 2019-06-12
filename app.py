78057bb34a6367b68a74fb9761db19800ec08848
mine444 <51392663+mine444@users.noreply.github.com>
2019-06-12 10:07:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
-
+#    json_data = {'Hello': 'World!'}
+    #return jsonify(json_data)
+    return "มิ้น 21 4/1"
 
 if __name__ == '__main__':
     app.run()
